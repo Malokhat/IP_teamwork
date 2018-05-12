0f7e797b02d402e10ca1a15bfbf451f10bb262c5
Malokhat <36439520+Malokhat@users.noreply.github.com>
2018-05-12 19:03:17 +0500

    Revert "Merge branch 'master' of https://github.com/Malokhat/IP_teamwork"
    This reverts commit 542915d92992c3be825afd25120ee0c6521ceab0, reversing
    changes made to 71b3bdfd342ce6f5c48336b1319c5672a1af396d.

--- js/script.js
+++ js/script.js
@@ -1,20 +1,46 @@
 
 
-$(function(){
-	var url = $("#upcomingTourCarousel .carousel-item.active").data("img");
-	$(".upcomingTours").css("background","transparent url("+url+") no-repeat center center");
-	$("#upcomingTourCarousel").on('slide.bs.carousel', function (e) {
-		var url = $(e.relatedTarget).data("img")
-		$(".upcomingTours").css("background","transparent url("+url+") no-repeat center center");
+window.onload = function(){
+	// var btn = document.querySelector('.bars');
+	// var menu = document.querySelector('.menu_sm');
+	// btn.onclick = function(){
+	// 	menu.classList.toggle('open');
+	// 	return false;
+	// }
 
-	});
+	// var div = document.querySelector('.nav_sm .menu_block');
+	// var h_window = window.outerHeight -200;
+	// function parallax(){
+	// 	var scroll = window.pageYOffset;
+	// 	console.log(scroll >= h_window);
+	// 	if(scroll >= h_window){
+	// 		div.classList.add('fixed');
+	// 	}else{
+	// 		div.classList.remove('fixed');
+	// 		// menu.classList.toggle('open');
+	// 	}
+	// }
+	// parallax();
+	// window.onscroll = function(){
+	// 	parallax();
+	// }
+
+}
+$(function(){
 	changeBackGraound();
+	$("#upcomingTourCarousel").on('slide.bs.carousel', function (e) {
+		changeBackGraound();
+	});
 });
 
+function changeBackGraound(){
+	var url = $("#upcomingTourCarousel .carousel-item.active").data("img");
+	$(".upcomingTours").css("background","transparent url("+url+") no-repeat center center");
+}
 $(window).scroll(function(){
 	var sticky = $('header'),
 		scroll = $(window).scrollTop();
   
-	if (scroll >= 650) sticky.addClass('fixed-header');
+	if (scroll >= 450) sticky.addClass('fixed-header');
 	else sticky.removeClass('fixed-header');
   });
